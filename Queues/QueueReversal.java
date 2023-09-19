7a49b3351d1f2f3d48d8ce6b9caf91dcc62cf249
Emkay2309 <emkay2309@gmail.com>
2023-09-19 14:18:04 +0530

    QueueReversal - Changed the input

--- Queues/QueueReversal.java
+++ Queues/QueueReversal.java
@@ -11,8 +11,9 @@ public class QueueReversal {
         q.add(1);
         q.add(2);
         q.add(3);
-        q.add(4);
+//        q.add(4);
         q.add(5);
+        q.add(6);
 
         Stack<Integer>  s = new Stack <>();
 

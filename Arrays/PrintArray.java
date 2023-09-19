05e59195d153d2ef9e7a90869c71d315439ab651
Emkay2309 <emkay2309@gmail.com>
2023-09-19 14:14:51 +0530

    New File Created in Arrays - Commit 1

---
+++ Arrays/PrintArray.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3};
+        for(int e : arr) {
+            System.out.print(e+ " ");
+        }
+    }
+}

6f429aa1970b3e9aba68db6e800a6ac0279b8174
Emkay2309 <emkay2309@gmail.com>
2023-09-22 02:10:27 +0530

    QueueReversal - Changed the input

---
+++ Strings/Pattern_Matching/Longest_Prefix_Suffix.java
@@ -0,0 +1,31 @@
+package Strings.Pattern_Matching;
+
+import java.util.Arrays;
+
+public class Longest_Prefix_Suffix {
+    public static void main(String[] args) {
+        String s = "aabacaaba";
+        int [] lps = lps(s);
+        System.out.println(Arrays.toString(lps));
+    }
+
+    private static int[] lps(String s) {
+        int n = s.length();
+        int [] ans = new int [n];
+
+        ans[0] = 0;
+
+        for(int i=1 ; i<n ; i++) {
+            int x = ans[i-1];
+            while(s.charAt(x) != s.charAt(i)) {
+                if(x == 0) {
+                    x = -1;
+                    break;
+                }
+                x = ans[x-1];
+            }
+            ans[i] = x+1;
+        }
+        return ans;
+    }
+}
